0dcb9090ee365706f8e273b1e4c27297fb3c4642
Juan <juan54driver@gmail.com>
2026-03-03 17:30:37 -0600

    programa main para un puesto de malteadas

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+def malteadas():
+    while True:
+        # Mostrar menu
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            #Se tiene que mostrar el menú
+            pass
+        elif opcion == "2":
+            # Tiene que mostrar el historial de las malteadas compradas
+            pass
+        elif opcion == "3":
+            print("Gracias por su preferencia, vuela pronto.")
+            break
+        else:
+            print("Opción no válida, intente con alguna de las opciones propuestas.")
+
+
+if __name__ == "__main__":
+    malteadas()
